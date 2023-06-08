e944db1c8826f6fab9d611980083d2b2b72d85a4
Sandeep Edara <edarasp@mail.uc.edu>
2023-06-08 14:08:30 -0400

    install dev tools and cache server data

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ErrorBoundary } from './container/ErrorBoundary';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry(failureCount: number, error: any) {
+        if (error.status === 404) { return false; }
+        else if (failureCount < 2) { return true; }
+        return false;
+      },
+      staleTime: 1000 * 60 * 60,
+      useErrorBoundary: true,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById(`root`) as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <App />
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
