2d3c6524028a9f9775e340acd0e9ae2a3bed722c
Sandeep Edara <edarasp@mail.uc.edu>
2023-06-08 14:10:02 -0400

    new types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -9,4 +9,31 @@ export interface GeoCodingResult {
   };
 }
 
-// export interface
+export interface ForecastApiUrlResult {
+  properties: {
+    forecast: string;
+    forecastHourly: string;
+  };
+}
+
+export interface ForecastResult {
+  properties: {
+    periods: Array<{
+      name: string;
+      startTime: string;
+      endTime: string;
+      isDaytime: boolean;
+      temperature: number;
+      temperatureUnit: string;
+      windSpeed: string;
+      windDirection: string;
+      relativeHumidity: {
+        value: number;
+        unitCode: string;
+      };
+      icon: string;
+      shortForecast: string;
+      detailedForecast: string;
+    }>;
+  };
+}
