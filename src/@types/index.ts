65b8cdb38bd627aea8b03c491e259bd9670acf0f
Sandeep Edara <edarasp@mail.uc.edu>
2023-06-08 23:32:18 -0400

    seperate type

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -16,24 +16,27 @@ export interface ForecastApiUrlResult {
   };
 }
 
+export interface Periods {
+  number: number;
+  name: string;
+  startTime: string;
+  endTime: string;
+  isDaytime: boolean;
+  temperature: number;
+  temperatureUnit: string;
+  windSpeed: string;
+  windDirection: string;
+  relativeHumidity: {
+    value: number;
+    unitCode: string;
+  };
+  icon: string;
+  shortForecast: string;
+  detailedForecast: string;
+}
+
 export interface ForecastResult {
   properties: {
-    periods: Array<{
-      name: string;
-      startTime: string;
-      endTime: string;
-      isDaytime: boolean;
-      temperature: number;
-      temperatureUnit: string;
-      windSpeed: string;
-      windDirection: string;
-      relativeHumidity: {
-        value: number;
-        unitCode: string;
-      };
-      icon: string;
-      shortForecast: string;
-      detailedForecast: string;
-    }>;
+    periods: Periods[];
   };
 }
